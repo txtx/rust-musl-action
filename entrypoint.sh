3f0c5d2c3abc22f2d21ec9b9437dca9e44198cd9
gmiam <63231406+gmiam@users.noreply.github.com>
2021-01-13 16:04:46 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@ set -Eeuxo pipefail
 # cp -rf /.cargo $GITHUB_WORKSPACE/
 # chmod -R 777 $GITHUB_WORKSPACE/.cargo
 cd $GITHUB_WORKSPACE
-rustup target add x86_64-unknown-linux-musl --toolchain stable-x86_64-unknown-linux-gnu
+rustup default stable
+rustup target add x86_64-unknown-linux-musl
 bash -c "$*"
