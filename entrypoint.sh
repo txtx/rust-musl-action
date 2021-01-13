c7fe607ce49c227b4310bd5e2c60ffbfc96c279d
gmiam <63231406+gmiam@users.noreply.github.com>
2021-01-13 16:14:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -Eeuxo pipefail
-# cp -rf /.cargo $GITHUB_WORKSPACE/
-# chmod -R 777 $GITHUB_WORKSPACE/.cargo
 cd $GITHUB_WORKSPACE
 rustup default stable
 rustup target add x86_64-unknown-linux-musl
